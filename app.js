8da49934a1338089363739fcb74c7ce24a9c5463
Felipe <felipe.tiburcio@gmail.com>
2024-07-05 21:41:56 -0300

    mysql database connection

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
+require("dotenv").config();
+
 const express = require("express");
-
 const app = express();
-
 const PORT = 8000;
+
+const mysql = require("mysql2");
+
+const { HOST, USER, PASSWORD, DATABASE } = process.env;
+
+const connection = mysql.createConnection({
+  host: HOST,
+  user: USER,
+  password: PASSWORD,
+  database: DATABASE,
+});
+
+connection.connect((err) => {
+  err ? console.error(err) : console.log("Connection Successful");
+});
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello, Node!</h1>");
