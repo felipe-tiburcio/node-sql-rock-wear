dac886dd09415a3d41f7954070e7cb7369cda0cc
Felipe <felipe.tiburcio@gmail.com>
2024-07-05 21:13:44 -0300

    express and nodemon

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Ok!");
+const express = require("express");
+
+const app = express();
+
+const PORT = 8000;
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello, Node!</h1>");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at port ${PORT}`);
+});
