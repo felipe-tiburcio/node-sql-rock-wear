3ce25b4052e6242e276a3fde8ca43e59a9e1f0c9
Felipe Tiburcio <felipe.tiburcio@gmail.com>
2024-08-05 20:39:31 -0300

    sql change to lowercase search terms

--- services/productService.js
+++ services/productService.js
@@ -3,187 +3,187 @@ const fs = require("fs");
 const path = require("path");
 
 const replaceImg = (img, req, res) => {
-  const imgBasePath = path.join(__dirname, "..") + "/public/img/";
-  const uploadImg = req.files?.image || "";
-  const newImage = uploadImg?.name || "";
+	const imgBasePath = path.join(__dirname, "..") + "/public/img/";
+	const uploadImg = req.files?.image || "";
+	const newImage = uploadImg?.name || "";
 
-  if (img !== newImage && img !== "" && newImage !== "") {
-    fs.unlink(`${imgBasePath}/${img}`, (err) => {
-      if (err) {
-        throw err;
-      }
+	if (img !== newImage && img !== "" && newImage !== "") {
+		fs.unlink(`${imgBasePath}/${img}`, (err) => {
+			if (err) {
+				throw err;
+			}
 
-      console.log("Image removed");
-    });
+			console.log("Image removed");
+		});
 
-    uploadImg.mv(imgBasePath + newImage);
-    console.log("New image copied to img folder");
-  }
+		uploadImg.mv(imgBasePath + newImage);
+		console.log("New image copied to img folder");
+	}
 
-  if (img === "") {
-    uploadImg.mv(imgBasePath + newImage);
-    console.log("New image copied to img folder");
-  }
+	if (img === "") {
+		uploadImg.mv(imgBasePath + newImage);
+		console.log("New image copied to img folder");
+	}
 };
 
 const saveForm = (req, res) => {
-  res.render("form");
+	res.render("form");
 };
 
 const saveFormWithStatus = (req, res) => {
-  const { status } = req.params;
+	const { status } = req.params;
 
-  res.render("form", { status: status });
+	res.render("form", { status: status });
 };
 
 const updateForm = (req, res) => {
-  const { id } = req.params;
+	const { id } = req.params;
 
-  let sqlSelect = `SELECT * FROM products WHERE ID = ?`;
+	let sqlSelect = `SELECT * FROM products WHERE ID = ?`;
 
-  connection.query(sqlSelect, [id], (err, ret) => {
-    if (err) {
-      throw err;
-    }
+	connection.query(sqlSelect, [id], (err, ret) => {
+		if (err) {
+			throw err;
+		}
 
-    const { id, name, price, image } = ret[0];
+		const { id, name, price, image } = ret[0];
 
-    res.render("update", { id, name, price, image });
-  });
+		res.render("update", { id, name, price, image });
+	});
 };
 
 const listProducts = (req, res) => {
-  const { category } = req.params;
+	const { category } = req.params;
 
-  let sql = "";
+	let sql = "";
 
-  if (category === "all") {
-    sql = `SELECT * from products ORDER BY RAND()`;
-  } else {
-    sql = `SELECT * FROM products WHERE CATEGORY = '${category}' ORDER BY name`;
-  }
+	if (category === "all") {
+		sql = `SELECT * from products ORDER BY RAND()`;
+	} else {
+		sql = `SELECT * FROM products WHERE CATEGORY = '${category}' ORDER BY name`;
+	}
 
-  connection.query(sql, (err, ret) => {
-    if (err) {
-      throw err;
-    }
+	connection.query(sql, (err, ret) => {
+		if (err) {
+			throw err;
+		}
 
-    res.render("list", { products: ret });
-  });
+		res.render("list", { products: ret });
+	});
 };
 
 const search = (req, res) => {
-  const { word } = req.body;
+	const { word } = req.body;
 
-  const sql = `SELECT * FROM products 
-               WHERE name LIKE ? 
+	const sql = `SELECT * FROM products 
+               WHERE LOWER(name) LIKE ? 
                OR price LIKE ?
                OR category LIKE ?`;
 
-  connection.query(sql, [`%${word}%`, `%${word}%`, `%${word}%`], (err, ret) => {
-    const noData = ret.length === 0;
+	connection.query(sql, [`%${word}%`, `%${word}%`, `%${word}%`], (err, ret) => {
+		const noData = ret.length === 0;
 
-    if (err) {
-      throw err;
-    }
+		if (err) {
+			throw err;
+		}
 
-    res.render("list", { products: ret, isRemoved: true });
-  });
+		res.render("list", { products: ret, isRemoved: true });
+	});
 };
 
 const saveProduct = (req, res) => {
-  try {
-    const { name, price, category } = req.body;
-    const img = req.files?.image?.name || "";
+	try {
+		const { name, price, category } = req.body;
+		const img = req.files?.image?.name || "";
 
-    if (
-      name === "" ||
-      price === "" ||
-      isNaN(price) ||
-      category === "" ||
-      img === ""
-    ) {
-      res.render("form", { isError: true });
-    } else {
-      const sql = `INSERT INTO products(name, price, category, image) VALUES(?, ?, ?, ?)`;
+		if (
+			name === "" ||
+			price === "" ||
+			isNaN(price) ||
+			category === "" ||
+			img === ""
+		) {
+			res.render("form", { isError: true });
+		} else {
+			const sql = `INSERT INTO products(name, price, category, image) VALUES(?, ?, ?, ?)`;
 
-      connection.query(sql, [name, price, category, img], (err, ret) => {
-        if (err) {
-          throw err;
-        }
+			connection.query(sql, [name, price, category, img], (err, ret) => {
+				if (err) {
+					throw err;
+				}
 
-        req.files.image.mv(path.join(__dirname, "../public/img/" + img));
-      });
+				req.files.image.mv(path.join(__dirname, "../public/img/" + img));
+			});
 
-      res.render("form", { isDone: true });
-    }
-  } catch (error) {
-    console.log(error);
-  }
+			res.render("form", { isDone: true });
+		}
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 const removeProduct = (req, res) => {
-  try {
-    const id = req.params.id;
+	try {
+		const id = req.params.id;
 
-    const sql = `DELETE FROM products WHERE ID = ?`;
+		const sql = `DELETE FROM products WHERE ID = ?`;
 
-    connection.query(sql, [id], (err, ret) => {
-      if (err) {
-        throw err;
-      }
+		connection.query(sql, [id], (err, ret) => {
+			if (err) {
+				throw err;
+			}
 
-      fs.unlink(`${__dirname}/public/img/${req.params.image}`, (err) => {
-        if (err) {
-          console.log(`Error removing image: ${err}`);
-        }
+			fs.unlink(`${__dirname}/public/img/${req.params.image}`, (err) => {
+				if (err) {
+					console.log(`Error removing image: ${err}`);
+				}
 
-        console.log("Successfully image removal");
-      });
-    });
+				console.log("Successfully image removal");
+			});
+		});
 
-    res.render("form", { isRemoved: true });
-  } catch (error) {
-    res.render("form", { isError: true });
-  }
+		res.render("form", { isRemoved: true });
+	} catch (error) {
+		res.render("form", { isError: true });
+	}
 };
 
 const updateProduct = (req, res) => {
-  const { id, name, price, image, category } = req.body;
-  const newImage = req.files?.image?.name || "";
-  let sqlUpdate,
-    requiredData = null;
+	const { id, name, price, image, category } = req.body;
+	const newImage = req.files?.image?.name || "";
+	let sqlUpdate,
+		requiredData = null;
 
-  if (name === "" || price === "" || isNaN(price)) {
-    res.redirect("/error");
-  } else {
-    if (newImage !== "" && category !== "") {
-      sqlUpdate = `UPDATE products SET name = ?, price = ?, image = ?, category = ? WHERE ID = ?;`;
-      requiredData = [name, price, newImage, category, id];
+	if (name === "" || price === "" || isNaN(price)) {
+		res.redirect("/error");
+	} else {
+		if (newImage !== "" && category !== "") {
+			sqlUpdate = `UPDATE products SET name = ?, price = ?, image = ?, category = ? WHERE ID = ?;`;
+			requiredData = [name, price, newImage, category, id];
 
-      replaceImg(image, req, res);
-    } else {
-      sqlUpdate = `UPDATE products SET name = ?, price = ? WHERE ID = ?;`;
-      requiredData = [name, price, id];
-    }
+			replaceImg(image, req, res);
+		} else {
+			sqlUpdate = `UPDATE products SET name = ?, price = ? WHERE ID = ?;`;
+			requiredData = [name, price, id];
+		}
 
-    connection.query(sqlUpdate, requiredData, (err, ret) => {
-      if (err) {
-        throw err;
-      }
+		connection.query(sqlUpdate, requiredData, (err, ret) => {
+			if (err) {
+				throw err;
+			}
 
-      res.render("form", { isUpdated: true });
-    });
-  }
+			res.render("form", { isUpdated: true });
+		});
+	}
 };
 
 module.exports = {
-  saveForm,
-  saveFormWithStatus,
-  updateForm,
-  listProducts,
-  search,
-  saveProduct,
-  removeProduct,
-  updateProduct,
+	saveForm,
+	saveFormWithStatus,
+	updateForm,
+	listProducts,
+	search,
+	saveProduct,
+	removeProduct,
+	updateProduct,
 };
